c7ebfb9a240b720df4b89cda648c0f7368fa487b
imcnaney <ian.mcnaney@gmail.com>
2013-09-04 05:18:05 -0600

    Playtime examples from the end of chapter 8 - added functional tests for the /products listing

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,15 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+
+    assert_select "#main .list_line_odd", 2
+    assert_select "#main .list_line_even", 1
+    assert_select "#main .list_description", 3
+    assert_select "#main .list_image", 3
+    assert_select "#main a", "Show", 3
+    assert_select "#main a", "Edit", 3
+    assert_select "#main .list_description dl dt", "Programming Ruby 1.9"
+    assert_select "#main .list_description dl dd", "MyText"
   end
 
   test "should get new" do
